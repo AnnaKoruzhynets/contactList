a8d9a00e9f1eb90a3559c3a534a71ba6d0a662fd
AnnaKoruzhynets <121451653+AnnaKoruzhynets@users.noreply.github.com>
2025-04-18 21:55:02 +0300

    ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,57 +1,53 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { addContact } from '../../redux/contactsSlice';
 import './ContactForm.css';
 
 export const ContactForm = () => {
-  const dispatch = useDispatch();
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
+	const dispatch = useDispatch();
+	const [name, setName] = useState('');
+	const [number, setNumber] = useState('');
 
-  const submit = e => {
-    e.preventDefault();
+	const handleSubmit = e => {
+		e.preventDefault();
+		if (name.trim() && number.trim()) {
+			dispatch(
+				addContact({
+					name: name,
+					number: number,
+					id: Date.now(),
+				})
+			);
+			setName('');
+			setNumber('');
+		}
+	};
 
-    if (name.trim() && number.trim()) {
-      dispatch(
-        addContact({
-          name: name,
-          number: number,
-          id: Date.now()
-        })
-      );
-      setName('');
-      setNumber('');
-    };
-  };
+	return (
+		<form onSubmit={handleSubmit} className='contact-form'>
+			<div className='input-group-contact'>
+				<input
+					type='text'
+					value={name}
+					onChange={e => setName(e.target.value)}
+					placeholder='Імя контакту'
+					className='input'
+					required
+				/>
 
-  return (
-    <form onSubmit={submit} className='contactForm'>
-      <div className='contactGroup'>
-        <input
-          type="text"
-          value={name}
-          onChange={e => setName(e.target.value)}
-          placeholder='Введіть ім"я'
-          className='input'
-          required
-        />
+				<input
+					type='number'
+					value={number}
+					onChange={e => setNumber(e.target.value)}
+					placeholder='Номер телефону'
+					className='input'
+					required
+				/>
+			</div>
 
-        <input
-          type="number"
-          value={number}
-          onChange={e => setNumber(e.target.value)}
-          placeholder='Введіть номер телефону'
-          className='input'
-          required
-        />
-
-        <button type='submit' className='btn'>
-          Додати контакт
-        </button>
-
-      </div>
-    </form>
-  );
+			<button type='submit' className='button'>
+				Додати контакт
+			</button>
+		</form>
+	);
 };
-
-
