af36524b5f64b5a6dfb7369a69816ba9378276f6
AnnaKoruzhynets <121451653+AnnaKoruzhynets@users.noreply.github.com>
2025-04-18 21:58:20 +0300

    store.js

--- redux/store.js
+++ redux/store.js
@@ -2,10 +2,8 @@ import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contactsSlice';
 
 export const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-  },
+	reducer: {
+		contacts: contactsReducer,
+	},
 });
-
-
 
